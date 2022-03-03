49b1a8f3aa35f1669e3b5d81952dafa6f7b28d4f
KyleMo <monstadkyle@gmail.com>
2022-03-03 09:59:05 -0800

    Played around a lot and learned a lot. All the code I added has mostly been commented out. Pushing most up to date to deploy simple one pager site to Heroku.

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 const router = express.Router();
+const PORT = 8080;
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+router.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname+'/views'+'pages'+'index.html'));
+})
 
-});
+/*
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views')) //if i were to run my app from outside the Gaming Project dir, this line would still allow the app to run
+app.use(express.static("public"));                                       // __dirname refers to the current dir of index.js
 
-app.use('/', router);
-app.listen(process.env.port || 3000);
+app.get('/', (req, res) => {
+  res.render('homepage.ejs') // by default render looks in the views folder
+})
+
+app.get('/g/:game', (req, res) => {
+  //const {game} = req.params
+  let gameTitle = "";
+  if(req.params.game.includes("-")){
+    gameTitle = req.params.game
+      .split("-")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+  else {
+    gameTitle = req.params.game;
+  }
+
+  res.render('homepage.ejs', {"game": gameTitle}) // by default render looks in the views folder
+})
+*/
+/*
+ Practice stuff with Colt Steele's learning course
+
+ app.get('/r/:subreddit', (req, res) => {
+   const {subreddit} = req.params;
+   res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
+ })
+
+ app.get('/r/:subreddit/:postID', (req, res) => {
+   const {subreddit, postID} = req.params;
+   res.send(`<h1>Viewing post ${postID} on the ${subreddit} subreddit</h1>`)
+ })
+
+ app.get('/search', (req, res) => {
+   const { q } = req.query;
+   res.send(`<h1>Query is ${q}</h1>`)
+ })
+
+ app.get('/cats', (req, res) => {
+   res.send("meow")
+ })
+
+ app.post('/cats', (req, res) => {
+   res.send("Post request to /cats!")
+ })
+
+ app.get('/dogs', (req, res) => {
+   res.send('woof')
+ })
+
+  app.get('*', (req, res) => { // captures all . This needs to be put last because routes are matched in order
+    console.log(`I dont know that path`)
+ })
+
+*/
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
