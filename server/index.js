4b263831478ab254a8b7456325a6b20bd20a7fd9
KyleMo <monstadkyle@gmail.com>
2022-05-05 19:29:24 -0700

    I HAVE FINALLY SOLVED MY BUG. .env file was in the wrong location. Would be very obvious bug to some one experienced, but now I know.

--- server/index.js
+++ server/index.js
@@ -2,29 +2,23 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const axios = require('axios');
+const bodyParser = require('body-parser');
 const gameDataRouter = require('./routes/gameData.js')
 const PORT = process.env.PORT || 8080;
 require('dotenv').config();
-//const cors = require('cors');
 
 
-
-//app.use(cors())
-
-app.use(express.static(path.resolve(__dirname, '../client/build')))
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/data',(req,res) => {
-  //const platform = 'origin';
-  //const platformUserIdentifier = 'chaoticbutpc';
-
-  const passedPlatform = req.query.platform;
-  const passedUsername = req.query.username;
-
+  const passedPlatform = "origin"//req.query.platform;
+  const passedUsername = "KeonTwitch"//req.query.username;console.log(process.env);
   axios.get(`https://public-api.tracker.gg/v2/apex/standard/profile/${passedPlatform}/${passedUsername}/sessions`,{
-    headers: {"TRN-Api-Key": process.env.REACT_APP_RAPID_API_KEY}
+    headers: {"TRN-Api-Key": process.env.REACT_APP_RAPID_API_KEY,
+              "Accept": "application/json",
+              "Accept-Encoding": "gzip"}
   })
     .then(response => {
-      //console.log(response.data.data.items)
       let matches = []
       const sessions = response.data.data.items;
       for (let i = 0; i < sessions.length; i++){
@@ -33,11 +27,14 @@ app.get('/data',(req,res) => {
             matches.push(sessions[i].matches[j]);
         }
       }
-      res.json(matches) //This should be pushing the data to the user
+      res.json(matches);
+
     })
     .catch(error => {
+
       res.json(error)
     });
+
 })
 
 
