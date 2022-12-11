059f717345fb4d98438897d8fd6c4a81fa7f4690
KyleMo <monstadkyle@gmail.com>
2022-12-10 21:35:37 -0800

    lint

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 
-import express from 'express';
-import path from 'path';
-import dataRoutes from './routes/data.js';
+import express from 'express'
+import path from 'path'
+import dataRoutes from './routes/data.js'
 import userRoutes from './routes/users.js'
-import {} from 'dotenv/config';
-import {fileURLToPath} from 'url';
+import {} from 'dotenv/config'
+import { fileURLToPath } from 'url'
 import connectDB from './config/db.js'
-import { notFound, errorHandler} from './middlewares/errorMiddleware.js'
-
+import { errorHandler } from './middlewares/errorMiddleware.js'
 
 const PORT = process.env.PORT || 8080;
 const __filename = fileURLToPath(import.meta.url)
@@ -20,11 +19,11 @@ app.use(express.json())
 
 app.use('/api/data', dataRoutes);
 app.use('/api/users', userRoutes);
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 app.use(errorHandler)
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
 })
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
